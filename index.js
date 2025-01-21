77f41db46e86afc9c0d24133da47c092224a4e4b
Michelle Villavilla <mchll.vllvll@gmail.com>
2025-01-21 22:14:48 +0800

    testing with cors

--- index.js
+++ index.js
@@ -1,45 +1,27 @@
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send({ message: 'Only POST requests allowed. on test 1' });
-  }
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-  const { to, subject, text, isRegistration, userEmail, userName, password, link } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'vehiqrspot@gmail.com',
-      pass: 'mlhaoacxxokjkruy', 
-    },
-  });
-
-  try {
-    if (isRegistration) {
-      await sendRegistrationEmail(userEmail, userName, password, link, subject, text, transporter);
-    } else {
-      await transporter.sendMail({
-        from: 'vehiqrspot@gmail.com',
-        to,
-        subject,
-        text,
-      });
-    }
-
-    res.status(200).json({ message: 'Email sent successfully!' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error sending email', error });
-  }
-}
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'vehiqrspot@gmail.com',
+    pass: 'mlhaoacxxokjkruy',
+  },
+});
 
 const sendRegistrationEmail = async (userEmail, userName, password, link, subject, text, transporter) => {
   try {
     await transporter.sendMail({
       from: 'vehiqrspot@gmail.com',
       to: userEmail,
-      subject: subject, // Use subject from frontend
-      text: text, // Use dynamic content from frontend
+      subject: subject,
+      text: text,
     });
     console.log('Registration email sent successfully');
     return true;
@@ -48,3 +30,30 @@ const sendRegistrationEmail = async (userEmail, userName, password, link, subjec
     return false;
   }
 };
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests allowed. testingggg 1' });
+  }
+
+  cors(corsOptions)(req, res, async () => {
+    const { to, subject, text, isRegistration, userEmail, userName, password, link } = req.body;
+
+    try {
+      if (isRegistration) {
+        await sendRegistrationEmail(userEmail, userName, password, link, subject, text, transporter);
+      } else {
+        await transporter.sendMail({
+          from: 'vehiqrspot@gmail.com',
+          to,
+          subject,
+          text,
+        });
+      }
+
+      res.status(200).json({ message: 'Email sent successfully!' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error sending email', error: error.message });
+    }
+  });
+}
